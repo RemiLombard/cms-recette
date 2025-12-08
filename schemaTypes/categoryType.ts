ba04e21f0982f5bb0fd3e133514a0c3a9feed814
Rémi <remi.lombard@edu.univ-fcomte.fr>
2025-12-08 14:59:22 +0100

    init

---
+++ schemaTypes/categoryType.ts
@@ -0,0 +1,20 @@
+import {defineField, defineType} from 'sanity'
+
+export const categoryType = defineType({
+  name: 'category',
+  title: 'Category',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'label',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'label'},
+      validation: (rule) => rule.required(),
+    }),
+  ],
+})
