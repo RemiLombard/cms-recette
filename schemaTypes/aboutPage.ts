758a5608df64f4926d4b8e2026e0870ffc95b6f8
Rémi <remi.lombard@edu.univ-fcomte.fr>
2025-12-22 00:12:41 +0100

    feat(about) : add about document

---
+++ schemaTypes/aboutPage.ts
@@ -0,0 +1,31 @@
+import {defineField, defineType} from 'sanity'
+
+export const aboutPage = defineType({
+  name: 'aboutPage',
+  title: 'About Page',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Titre',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+
+    defineField({
+      name: 'content',
+      title: 'Contenu',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+})
